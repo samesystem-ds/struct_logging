cbd00290f0a873aaf21b6f7669ab6a5d6f757983
linas-samesystem <50135698+linas-samesystem@users.noreply.github.com>
2022-02-17 08:38:26 +0200

    license

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="struct_logging",
+    version="0.0.1",
+    author="SameSystem",
+    author_email="linas.ziedas@samesystem.com",
+    description="Struct logging for apps",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/samesystem-ds/struct_logging",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
